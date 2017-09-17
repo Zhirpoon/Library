106bc248287373752b9d561003cc7abac35e4ee7
Johan Rosengren <zhirpoon@gmail.com>
2017-09-17 21:36:19 +0200

    fix errors

--- Cudoviste/main.cpp
+++ Cudoviste/main.cpp
@@ -6,15 +6,19 @@ using namespace std;
 int main()
 {
     int r,c;
-    scanf("%d" "%d", &r, &c);
+    if(scanf("%d %d", &r, &c) != 2) {
+        exit(1);
+    }
     char lot[r][c+1];
     for(int i=0;i<r;i++) {
-        scanf("%s", lot[i]);
+        if(scanf("%s", lot[i]) != 1) {
+            exit(1);
+        }
     }
     int counters[] = {0,0,0,0,0};
     for(int i=0;i<r-1;i++) {
         for(int j=0;j<c-1;j++) {
-            if(lot[i][j] != '#' && lot[i][j+1] != '#' && lot[i+1][j] && lot[i+1][j+1] != '#') {
+            if(lot[i][j] != '#' && lot[i][j+1] != '#' && lot[i+1][j] != '#' && lot[i+1][j+1] != '#') {
                 counters[(lot[i][j] == 'X' ? 1:0) + (lot[i][j+1] == 'X' ? 1:0) + (lot[i+1][j] == 'X' ? 1:0) + (lot[i+1][j+1] == 'X' ? 1:0)]++;
             }
         }
